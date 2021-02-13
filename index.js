264c493ebed48c9495c3b8333947e06f2d93b81f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-02-13 04:27:35 -0500

    feat: change name to bilitube; fix bugs

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const css = hexo.extend.helper.get('css').bind(hexo);
-const js = hexo.extend.helper.get('css').bind(hexo);
+const js = hexo.extend.helper.get('js').bind(hexo);
 const { name, version } = require('./package.json');
+const { htmlTag } = require('hexo-util');
 const Injector = require("hexo-tag-injector");
 const injector = new Injector(hexo);
-const cdn_url = function(path){
-	return `https://cdn.jsdelivr.net/npm/${name}@${version}/${path}`;
+const cdn_url = function (path) {
+    return `https://cdn.jsdelivr.net/npm/${name}@${version}/${path}`;
 }
 
-hexo.extend.tag.register('youtubefallback', function (args) {
-    if (args[1] === 'end') {
-        return '</div>';
-    } else if (args[1] == 'begin') {
-        return `<div class="youtubefallback_${args[0]}">` + injector.mark('');
-    }
+hexo.extend.tag.register('bilitube', function (args) {
+    /** {% bilitube youtube_id bvid [loading_icon] %} */
+    return injector.mark(htmlTag(
+        'div',
+        { class: "bilitube video-container", "data-youtube": args[0], "data-bvid": args[1] },
+        args[2]? `<i class="${args[2]}"></i> Loading video...`: 'Loading video...',
+        false 
+    ));
 });
 
-injector.register('head_end', css({
-    href: cdn_url('src/youtubefallback.min.css'),
-    class: 'pjax',
-}));
 
 injector.register('body_end', js({
     href: cdn_url('src/youtubefallback.min.js'),
--- src/youtubefallback.js
+++ src/youtubefallback.js
@@ -1,13 +1,21 @@
 (() => {
-    /** When youtube is unavaiable, fallback to alternative video website */
+    /** When youtube is unavaiable, fallback to bilibili */
     const $$ = (className) => document.querySelectorAll(className);
+    const load_video = (element, vsrc) => {
+        const frame = `<iframe src="${vsrc}" frameborder="0" loading="lazy" allowfullscreen></iframe>`;
+        element.innerHTML = frame;
+    };
     function use_youtube() {
-        $$(".youtubefallback_message").forEach(element => { element.style.display = 'none'; });
-        $$(".youtubefallback_youtube").forEach(element => { element.style.display = 'unset'; });
+        // https://www.youtube.com/embed/diWEMGDKx9g
+        $$(".bilitube").forEach(element => {
+            load_video(element, `https://www.youtube.com/embed/${element.dataset.youtube}`);
+        });
     }
-    function use_fallback() {
-        $$(".youtubefallback_message").forEach(element => { element.style.display = 'none'; });
-        $$(".youtubefallback_fallback").forEach(element => { element.style.display = 'unset'; });
+    function use_bilibili() {
+        // https://player.bilibili.com/player.html?bvid=BV1454y1X7et
+        $$(".bilitube").forEach(element => {
+            load_video(element, `https://player.bilibili.com/player.html?bvid=${element.dataset.bvid}`);
+        });
     }
     function check_youtube() {
         const runcheck = (domain) => {
@@ -36,7 +44,7 @@
 
         return Promise.all([
             runcheck('www.youtube.com'),
-        ]).then(use_youtube, use_fallback);
+        ]).then(use_youtube, use_bilibili);
     }
     check_youtube();
 })();
