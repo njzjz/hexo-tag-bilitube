6b8b12c046c7253fdd4666ca84eb02ea8887ad52
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-19 15:15:37 -0400

    replace jsdelivr with unpkg
    see https://github.com/jsdelivr/jsdelivr/issues/18397

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { name, version } = require('bilitube/package.json');
 const { htmlTag } = require('hexo-util');
 const Injector = require("hexo-tag-injector");
 const injector = new Injector(hexo, id = name);
-const { npm_url } = require("jsdelivr_url");
+const { npm_url } = require("unpkg_url");
 
 hexo.extend.tag.register('bilitube', function (args) {
     /** {% bilitube youtube_id bvid [loading_icon] %} */
